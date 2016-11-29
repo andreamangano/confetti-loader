4074bda4ded202df1814d2b6188e2fd66c1ffe1f
AMangano <andreaman87@gmail.com>
2016-11-29 21:06:48 +0100

    Added try/catch on loadDeck() async method.

--- src/lib/loader.js
+++ src/lib/loader.js
@@ -4,74 +4,102 @@ import * as utils from './utils';
  The loader class aims to retrieve all data and settings from the config files.
  */
 class Loader {
-  constructor() {}
   /* Load deck settings */
   loadDeckConfig(path) {
-    return utils.loadYaml2JSON(path);
+    return new Promise((resolve, reject) => {
+      utils.loadYaml2JSON(path)
+        .then(data => {
+          // TODO: add check on file structure
+          // Check on properties on data object
+          // reject if something is wrong
+          resolve(data);
+        })
+        .catch(error => reject(error));
+    });
   }
+
   /* Load slides */
   loadSlides(path) {
-    return utils.loadYaml2JSON(path);
+    return new Promise((resolve, reject) => {
+      utils.loadYaml2JSON(path)
+        .then(data => {
+          // TODO: add check on file structure
+          // Check on properties on data object
+          // reject if something is wrong
+          resolve(data);
+        })
+        .catch(error => reject(error));
+    });
   }
-  /* Load translations */
-  loadTranslations(path, lang) {
-    return utils.loadYaml2JSON(
-      utils.getPathTranslationFile(path, lang)
-    );
-  }
+
   /* Load theme config */
   loadThemeConfig(path) {
-    return utils.loadYaml2JSON(path);
+    return new Promise((resolve, reject) => {
+      utils.loadYaml2JSON(path)
+        .then(data => {
+          // TODO: add check on file structure
+          // Check on properties on data object
+          // reject if something is wrong
+          resolve(data);
+        })
+        .catch(error => reject(error));
+    });
   }
   /*
    Structure of config parameter:
    {
-     paths: {
-       settings: 'path/to/settings.yml',
-       slides: 'path/to/slides.yml,
-       themes: 'path/to/themes'
-     },
-     compilers: {
-      sass: {...}
-     }
+   paths: {
+   settings: 'path/to/settings.yml',
+   slides: 'path/to/slides.yml,
+   themes: 'path/to/themes'
+   },
+   compilers: {
+   sass: {...}
+   }
    }
    */
   async loadDeck(config) {
     // TODO: check on the config structure
-    let themePath;
-    // Load base deck settings
-    const data = await this.loadDeckConfig(config.paths.settings);
-    // Added compilers
-    data.compilers = config.compilers;
-    // Load slide data
-    data.slides = await this.loadSlides(config.paths.slides);
-    // Base theme path
-    themePath = path.join(config.paths.themes, `confetti-theme-${data.theme}`);
-    // Set paths: sources and destinations
-    data.paths = {};
-    // Set theme source paths
-    data.paths.sources = {
-      languages: path.join(themePath, 'languages'),
-      views: path.join(themePath, 'views'),
-      styles: path.join(themePath, 'styles'),
-      fonts: path.join(themePath, 'fonts'),
-      images: path.join(themePath, 'images')
-    };
-    // Set destinations paths
-    data.paths.destinations = {
-      views: config.paths.dist,
-      styles: path.join(config.paths.dist, 'styles'),
-      fonts: path.join(config.paths.dist, 'fonts'),
-      images: path.join(config.paths.dist, 'images')
-    };
-    // Load theme configs
-    data.themeConfig = await this.loadThemeConfig(
-      path.join(themePath, 'data.yml')
-    ).config;
-    // Load translations
-    data.translations = await this.loadTranslations(
-      data.paths.sources.languages, data.lang
-    );
+    let data = null;
+    try {
+      let themePath;
+      // Load base deck settings
+      data = await this.loadDeckConfig(config.paths.settings);
+      // Added compilers
+      data.compilers = config.compilers;
+      // Load slide data
+      data.slides = await this.loadSlides(config.paths.slides).slides;
+      // Base theme path
+      themePath = path.join(config.paths.themes, `confetti-theme-${data.theme}`);
+      // Set paths: sources and destinations
+      data.paths = {};
+      // Set theme source paths
+      data.paths.sources = {
+        languages: path.join(themePath, 'languages'),
+        views: path.join(themePath, 'views'),
+        styles: path.join(themePath, 'styles'),
+        fonts: path.join(themePath, 'fonts'),
+        images: path.join(themePath, 'images')
+      };
+      // Set destinations paths
+      data.paths.destinations = {
+        views: config.paths.dist,
+        styles: path.join(config.paths.dist, 'styles'),
+        fonts: path.join(config.paths.dist, 'fonts'),
+        images: path.join(config.paths.dist, 'images')
+      };
+      // Load theme configs
+      data.themeConfig = await this.loadThemeConfig(
+        path.join(themePath, 'data.yml')
+      ).config;
+      // Load translations
+      data.translations = await utils.loadYaml2JSON(
+        utils.getPathTranslationFile(data.paths.sources.languages, data.lang)
+      );
+    } catch (err) {
+      console.log(err);
+      throw new Error(err);
+    }
     return data;
   }
 }
