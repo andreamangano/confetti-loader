2877acddb0485d4e355d8ba55445648f348021f4
AMangano <andreaman87@gmail.com>
2016-12-03 13:32:13 +0100

    Made bin file to use loader by cli.

---
+++ src/bin/loader.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+'use strict';
+import argv from 'yargs';
+import Loader from './../lib/loader';
+const usage = argv
+  .usage('Usage: $0 --config [file]')
+  .option('config', {
+    type: 'string',
+    describe: 'Path to the config file',
+    defaultDescription: 'confetti.loader.json',
+    requiresArg: true
+  })
+  .argv;
+
+const fs = require('fs');
+const stream = fs.createReadStream(usage.config);
+
+let content = '';
+stream.on('data', function(buf) {
+  content += buf.toString();
+});
+stream.on('end', function() {
+  const loaderConfig = JSON.parse(content);
+  const loader = new Loader();
+  loader.loadDeck(loaderConfig).then(
+    data => {
+      process.stdout.write(JSON.stringify(data));
+    }
+  );
+});
