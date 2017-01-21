f7a8da5edb88599606bb95d6d431e2e820e31415
AMangano <andreaman87@gmail.com>
2017-01-21 20:04:54 +0100

    Edits default deck data

--- src/lib/default-settings.js
+++ src/lib/default-settings.js
@@ -2,13 +2,10 @@
 // Default settings
 export default {
   lang: 'en_EN',
-  /*
-    Date / Time format
-    Confetti uses Moment.js to parse and display date
-    You can customize the date format as defined in
-    http://momentjs.com/docs/#/displaying/format/
-  */
-  date_format: 'YYYY-MM-DD',
-  theme: 'tangerine'
+  theme: 'voice',
+  meta: {
+    description: 'Slide deck created with Confetti.',
+    keywords: [ 'confetti', 'presentation', 'slide deck', 'speaker' ]
+  }
 };
 
